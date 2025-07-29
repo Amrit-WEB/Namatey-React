26636cd6ffb253218c853bbbf3bc81b2c709b7a9
Amrit-WEB <amritanshuprajapati42@gmail.com>
2025-07-29 11:00:06 +0530

    added quote api in about and install testing library

--- components/About.js
+++ components/About.js
@@ -1,7 +1,48 @@
+// function About() {
+//   return (
+//     <div>
+//       <h1>About Us</h1>
+//     </div>
+//   );
+// }
+
+// export default About;
+
+import { useState, useEffect } from "react";
+
 function About() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://thingproxy.freeboard.io/fetch/https://api.quotable.io/random"
+      );
+      // const response = await fetch("https://zenquotes.io/api/random");
+      const data = await response.json();
+      setQuote(data.content);
+      setAuthor(data.author);
+    } catch (error) {
+      setQuote("Problem with fetching quote");
+      console.log(error);
+    }
+  };
   return (
-    <div>
-      <h1>About Us</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "10px",
+        padding: "20px",
+      }}
+    >
+      <h1>{quote}</h1>
+      <h2 style={{ color: "red", fontSize: "15px" }}>{author}</h2>
+      <button onClick={fetchData}>Another</button>
     </div>
   );
 }
